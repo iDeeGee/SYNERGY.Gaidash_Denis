dc0ca65d93a993804473dbb75b9160f11f0d54bb
denisgaidash <denisgaidash@gmail.com>
2024-09-03 14:00:30 +0300

    Доработка 2

--- L8.GaidashDenis.py
+++ L8.GaidashDenis.py
@@ -1,48 +1,81 @@
 # Задание 1:
 N = int(input("Введите количество чисел N: "))
+
+if not (1 <= N <= 10000):
+    raise ValueError("N должно быть в диапазоне от 1 до 10000")
+
 numbers = []
 
-for _ in range(N):
-    number = int(input())
+for i in range(N):
+    number = int(input(f"Введите число {i + 1}: "))
+    
+    if abs(number) > 10**6:
+        raise ValueError("Недопустимые значения!")
+    
     numbers.append(number)
 
 reversed_numbers = numbers[::-1]
+
 print(*reversed_numbers)
 
 # Задание 2:
-num_N = int(input("Введите количество чисел N: "))
-array = list(map(int, input("Введите N чисел через пробел: ").split()))
+N = int(input("Введите количество чисел N: "))
 
-rearranged_array = [0] * num_N
+if not (1 <= N <= 100000):
+    raise ValueError("N должно быть в диапазоне от 1 до 100000")
 
-for i in range(num_N):
+input_numbers = input("Введите N чисел через пробел: ").split()
+
+if len(input_numbers) != N:
+    raise ValueError("Количество введенных чисел должно совпадать с N")
+
+numbers = []
+for num_str in input_numbers:
+    num = int(num_str)
+    if not (1 <= num <= 10**10):
+        raise ValueError("Недопустимое значение!")
+    numbers.append(num)
+
+rearranged_numbers = [0] * N
+
+for i in range(N):
     if i % 2 == 0:
-        rearranged_array[i] = array[-(i//2 + 1)]
+        rearranged_numbers[i] = numbers[-(i // 2 + 1)]
     else:
-        rearranged_array[i] = array[i//2]
+        rearranged_numbers[i] = numbers[i // 2]
 
-print(*rearranged_array)
+print(*rearranged_numbers)
+
 
 # Задание 3:
 m = int(input("Введите максимальную массу, которую может выдержать лодка: "))
+
+if not (1 <= m <= 10**7):
+    raise ValueError("Недопустимое значение!")
+
 n = int(input("Введите количество рыбаков: "))
 
+if not (1 <= n <= 100):
+    raise ValueError("Количество рыбаков должно быть от 1 до 100")
+
 weights = []
-for _ in range(n):
-    weights.append(int(input()))
+for i in range(n):
+    weight = int(input(f"Введите вес рыбака {i + 1}: "))
+    if not (1 <= weight <= m):
+        raise ValueError("Вес рыбака должен быть от 1 до m")
+    weights.append(weight)
 
 weights.sort()
 
-left = 0  
-right = n - 1  
-boats = 0 
+boats = 0  
+i = 0  
+j = n - 1  
 
-while left <= right:
+while i <= j:
+    if weights[i] + weights[j] <= m:
+        i += 1  
     
-    if weights[left] + weights[right] <= m:
-        left += 1  
-    
-    right -= 1  
-    boats += 1 
+    j -= 1
+    boats += 1  
 
-print(boats)
+print("Минимальное количество лодок: ", boats)
